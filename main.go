662f34305b91ec7744a9f7f5e9514f85ab24fcfb
Pavlin <pavling.georgiev@gmail.com>
2024-08-24 19:28:29 -0400

    bleh

--- main.go
+++ main.go
@@ -16,30 +16,26 @@ func main() {
 	for _, p := range []*store.Product{kayak, lifejacket} {
 		fmt.Println("Name", p.Name, "Category", p.Category, "Price", p.Price(0.2)) // Notice here p.Price is  the method Price() which adds a 20% tax to the price
 	}
-	/*
-		// STEP TWO
-		// Introduce second type
+	// STEP TWO
+	// Introduce second type
 
-		fmt.Printf("------------------- \n Boat Type \n")
-		boats := []*store.Boat{
-			store.NewBoat("Kayak", 275, 1, false),
-			store.NewBoat("Canoe", 400, 3, false),
-			store.NewBoat("Tender", 650.25, 1, true),
-		}
+	fmt.Printf("------------------- \n Boat Type \n")
+	boats := []*store.Boat{
+		store.NewBoat("Kayak", 275, 1, false),
+		store.NewBoat("Canoe", 400, 3, false),
+		store.NewBoat("Tender", 650.25, 1, true),
+	}
 
-		for _, b := range boats {
-			fmt.Println("Name", b.Name, "Capacity", b.Capacity, "Price: ", b.Price(0.2))
-		}
-		fmt.Printf("--------------\n  Interface \n")
-	*/
+	for _, b := range boats {
+		fmt.Println("Name", b.Name, "Capacity", b.Capacity, "Price: ", b.Price(0.2))
+	}
+	fmt.Printf("--------------\n  Interface \n")
 
 	// STEP THREE
 
 	// Time for Interfaces
 	// Having an Interface that uses the common Price method allows us to easily define structures with multiple types of structs
 	//	which share common methods
-
-	/* REMOVE HERE
 
 	products := map[string]store.ItemForSale{
 		"Kayak": store.NewBoat("Kayak", 279, 1, false),
@@ -50,16 +46,12 @@ func main() {
 		fmt.Println("Key", key, "Price :", p.Price(0.2))
 	}
 
-
-	*/ // AND REMOVE THIS LINE STEP 3
-
 	// STEP FOUR
 	//
 	// In order to acces the Product fields in products we need to separate the different types in our interface
 	// That can be done by first getting the type with p.(type) and then using a switch case to differentiate our output
 	//
 
-	/* // REMOVE HERE FOR STEP 4
 	for key, p := range products {
 		switch item := p.(type) {
 		case *store.Product:
@@ -72,5 +64,4 @@ func main() {
 		}
 	}
 
-	*/ // REMOVE HERE FOR STEP 4
 }
